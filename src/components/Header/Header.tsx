6e2b78da7f16da71e4d20de5a8d8991b8e3bd117
alicbm <abarandica1234@gmail.com>
2024-04-14 15:51:32 -0500

    vista del home completada, header modificado, variables en css aplicadas para colores globales

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,46 +1,78 @@
-import * as React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import './Header.styles.css';
+import * as React from "react";
+import logo from "../../assets/logogid.png";
+import "./Header.styles.css";
+import { useNavigate } from "react-router-dom";
 
 type loggedType = {
-  logged: boolean
-}
+  logged: boolean;
+};
 
-const Header: React.FC<loggedType> = ({logged}:loggedType)=>{
-  return(
-    <header>
-    <div className="wrapper">
-      <div className="header_container">
-        <div>
-          <h1>DreamHouse</h1>
-        </div>
-        <nav>
-          <ul>
-            {logged ? (
-              <Dropdown className='dropdown-btn'>
-                  <Dropdown.Toggle id="dropdown-basic">
-                      <div className="dropdown-text">
-                          <img src="./src/assets/account.svg" alt="user-img"/>
-                      </div>
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu className='dropdown-menu'>
-                      <Dropdown.Item href="#">Configuración</Dropdown.Item>
-                      <Dropdown.Item href="#">Cerrar Sesión</Dropdown.Item>
-                  </Dropdown.Menu>
-              </Dropdown>
-            ) : (
-              <>
-                <li>Login</li>
-                <li>Acerca de</li>
-              </>
-            )}
-          </ul>
-        </nav>
+type RoutesType = {
+  label: string;
+  route: string;
+  selected: boolean;
+};
+
+const routes: RoutesType[] = [];
+
+routes.push({
+  label: "Home",
+  route: "/",
+  selected: true,
+});
+
+routes.push({
+  label: "About us",
+  route: "/",
+  selected: false,
+});
+
+routes.push({
+  label: "Service",
+  route: "/",
+  selected: false,
+});
+
+routes.push({
+  label: "Contact",
+  route: "/",
+  selected: false,
+});
+
+const Header: React.FC<loggedType> = ({ logged }: loggedType) => {
+  const navigate = useNavigate();
+
+  const itemSelected = (label: string) => {
+    routes.map((item) =>
+      item.label != label ? (item.selected = false) : (item.selected = true)
+    );
+  };
+
+  return (
+    <header className="Header">
+      <div className="Header-logo">
+        <img src={logo} alt="DreamHome" className="Header-logo__log" />
+        <p className="Header-logo__text">DreamHome</p>
       </div>
-    </div>
-  </header>
-  )
-}
 
+      <ul className="Header-navbar">
+        {routes.map(({ label, route, selected }) => (
+          <li
+            className={`${
+              selected ? "header-selected" : ""
+            } Header-navbar__item`}
+            onClick={() => {
+              navigate(route);
+              itemSelected(label);
+            }}
+          >
+            {label}
+          </li>
+        ))}
+        <li className="Header-navbar__join">Join is</li>
+      </ul>
+    </header>
+  );
+};
 
 export default Header;
