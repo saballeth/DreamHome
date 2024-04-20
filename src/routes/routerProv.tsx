1b957bf15aa3e22551d313334a7f1b4fafd088ec
alicbm <abarandica1234@gmail.com>
2024-04-20 17:59:47 -0500

    vista de contactos y registro completadas

--- src/routes/routerProv.tsx
+++ src/routes/routerProv.tsx
@@ -1,27 +1,33 @@
-import AboutUs from '@/components/AboutUs/AboutUs';
-import { Home } from '@/pages/Home/Index';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutUs from "@/components/AboutUs/AboutUs";
+import { ContactUs } from "@/pages/ContactUs/ContactUs";
+import { CreateAccount } from "@/pages/CreateAccount/CreateAccount";
+import { Home } from "@/pages/Home/Index";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function RouterProv() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      Component: Home,
+      // errorElement: <ErrorPage/>
+    },
+    {
+      path: "/aboutus",
+      Component: AboutUs,
+      // errorElement: <ErrorPage/>
+    },
+    {
+      path: "/create-account",
+      Component: CreateAccount,
+    },
 
-    const routes = createBrowserRouter([
-        {
-            path: "/",
-            Component: Home,
-            // errorElement: <ErrorPage/>
-        },
-        {
-            path: "/aboutus",
-            Component: AboutUs,
-            // errorElement: <ErrorPage/>
-        },
+    {
+      path: "/contact-us",
+      Component: ContactUs,
+    },
+  ]);
 
-    ]);
-
-    return (
-        <RouterProvider router={routes}></RouterProvider>
-    )
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
-
-export default RouterProv
+export default RouterProv;
