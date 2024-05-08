02a1b4bc2c59b10f0dbb13c1c44c3e538ef524f3
diego oñate <diegoonate3026@gmail.com>
2024-05-08 17:02:39 -0500

    caracteristica vista

--- src/pages/Caracteristicas/caracteristicasInmuebles.tsx
+++ src/pages/Caracteristicas/caracteristicasInmuebles.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/Principal/Principal_Header/PrincipalHeader"
 import Caracteristicas from '@/components/Caracteristicas/Caracteristicas'
+import Footer from '@/components/Footer/Footer'
+
 const Login: React.FC = () =>{
   
     return (
         <>
-            <Header/>
-            <Caracteristicas/>
+            <Header colorNameLogo={true} colorUbi={true}/>
+            <Caracteristicas data={undefined}/>
+            <Footer/>
         </>
     )
 }
