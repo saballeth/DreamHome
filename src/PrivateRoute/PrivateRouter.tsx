10a54f47604e61cfaf5203edfd233b487859721a
JesiD <deivisalcedo.o@gmail.com>
2024-05-09 09:46:13 -0500

    antes del pull

---
+++ src/PrivateRoute/PrivateRouter.tsx
@@ -0,0 +1,18 @@
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
